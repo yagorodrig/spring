2277216a866874d889f0215a7103d15198cfb502
yagorodrig <53234295+yagorodrig@users.noreply.github.com>
2019-09-01 16:14:13 -0300

    Listando todas categorias com DTO

--- src/main/java/com/spring/curso/services/CategoriaService.java
+++ src/main/java/com/spring/curso/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.spring.curso.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +44,11 @@ public class CategoriaService {
 		}catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir uma categora que possui produtos");
 		}
+		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 		
 	}
 }
