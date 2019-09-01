dc4977e085e0c339768f19527c493f6385a2e283
yagorodrig <53234295+yagorodrig@users.noreply.github.com>
2019-09-01 17:22:51 -0300

    paginacao com parametros opcionais na requisicao

--- src/main/java/com/spring/curso/services/CategoriaService.java
+++ src/main/java/com/spring/curso/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.spring.curso.domain.Categoria;
@@ -50,5 +53,11 @@ public class CategoriaService {
 	public List<Categoria> findAll(){
 		return repo.findAll();
 		
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		@SuppressWarnings("deprecation")
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
